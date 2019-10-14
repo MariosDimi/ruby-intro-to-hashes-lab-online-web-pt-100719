66a3361be0a125f6f3b63061a3f34b306441ef3e
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-14 03:31:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,6 +35,16 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  
-
+  monopoly = {:airports
+	  :athens => 1,
+	  :newyork => 2
+	  {[:athens][:eleftheriosvenizelos]
+	  [:newyork][:jfk, :laguardia]
+	  [:athens][:eleftheriosvenizelos][:olympicairlines]
+	  [:athens][:jfk, :laguardia][:deltaairlines]
+	  {:newyork => "total_mortgage" => "$500B"}
+	  {:athens => "total_mortgage" => "$40M"}
+	  }
+  }
+ }
 end
